18c8201eb518bf27e8d7ac1f4d9b8f478a721ffe
Liz Kodjo <ellizakodjo@outlook.com>
2026-07-11 13:41:26 +0100

    Created style.py for branding so it can be used on different pages

---
+++ collect_data.py
@@ -0,0 +1,6 @@
+import requests
+import pandas as pd
+import os
+
+class DataCollector:
+    """Save data from APIs"""
---
+++ pages/4_🏙️_Ques2.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from utils.styles import apply_sidebar_styles
+
+apply_sidebar_styles()
+
